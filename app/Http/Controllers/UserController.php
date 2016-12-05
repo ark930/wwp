4fcb75ca2742885c19c2f7515106be2a3326d257
edwin <lark930@gmail.com>
2016-12-06 00:37:12 +0800

    从UserController.php移除登录、登出逻辑代码

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,53 +11,6 @@ use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'tel' => 'required|regex:/^1\d{10}$/',
-            'verify_code' => 'required|regex:/^\d{4}$/',
-        ], [
-            'tel.required' => '请填写手机号',
-            'tel.regex' => '请填写正确的手机号',
-            'verify_code.required' => '请填写验证码',
-            'verify_code.regex' => '验证码格式不正确',
-        ]);
-
-        $tel = $request->input('tel');
-        $verifyCode = $request->input('verify_code');
-
-        $user = User::where('tel', $tel)->first();
-
-        if(empty($user)) {
-            throw new BadRequestException('登录失败');
-        }
-
-        if($user->ifVerifyCodeExpired()) {
-            throw new BadRequestException('验证码过期, 请重新获取验证码');
-        }
-
-        if($user->ifVerifyCodeRetryTimesExceed()) {
-            throw new BadRequestException('验证码输入错误次数过多, 已失效, 请重新获取验证码');
-        }
-
-        if($user->ifVerifyCodeWrong($verifyCode)) {
-            throw new BadRequestException('验证码错误');
-        }
-
-        $user->disableVerifyCode();
-        $user->ifFirstLogin();
-        $user->updateLastLogin();
-
-        return response('', 200);
-    }
-
-    public function logout() {
-        Session::flush();
-        Session::regenerate();
-
-        return response('', 200);
-    }
-
     /**
      * 获取登录验证码
      *
