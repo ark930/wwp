9abcb93052efd3a60707db717ec8f09cc4db0b89
edwin <lark930@gmail.com>
2016-12-06 00:39:10 +0800

    文章发布、删除路由

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('articles', 'ArticleController');
     Route::post('articles/{article_id}/publish', 'ArticleController@publish');
     Route::post('articles/{article_id}/unpublish', 'ArticleController@unpublish');
+    Route::post('articles/{article_id}/trash', 'ArticleController@trash');
+    Route::post('articles/{article_id}/untrash', 'ArticleController@untrash');
     Route::post('articles/{article_id}/cover', 'UserController@uploadCover');
 
     Route::post('user/avatar', 'UserController@uploadAvatar');
