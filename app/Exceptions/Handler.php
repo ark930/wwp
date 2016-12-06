1e382276e2157f0f81469e3de8d492ce5a8a18d5
edwin <lark930@gmail.com>
2016-12-06 17:56:27 +0800

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,12 +52,11 @@ class Handler extends ExceptionHandler
             return response()->json(['msg' => '资源不存在'], 404);
         } else if ($exception instanceof TokenMismatchException){
             return response()->json(['msg' => '会话已过期, 请重新登录'], 401);
-        } else {
-            return response()->json(['msg' => '发生了一个错误'], 400);
-
+        } else if ($exception instanceof AuthenticationException) {
+            return response()->json(['msg' => '未授权'], 401);
         }
 
-//        return parent::render($request, $exception);
+        return parent::render($request, $exception);
     }
 
     /**
