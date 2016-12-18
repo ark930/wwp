2c9dcf9731cfda78459e3417b22a5e44359e88b1
edwin <lark930@gmail.com>
2016-12-18 21:22:27 +0800

    Add Comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+    use SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function article()
+    {
+        return $this->belongsTo('App\Models\Article');
+    }
+}
