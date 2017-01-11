1a59f5f690e5f1bc04d11f1fe5720f8f2f845b7f
edwin <lark930@gmail.com>
2017-01-11 22:16:14 +0800

    修复Admin无法查看数据的问题

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,22 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Device;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // 挡掉没权限的人
-        if(!Auth::check()) {
-            throw new AuthenticationException();
+        $isAdmin = false;
+        if($request->session()->has('device_id')) {
+            $device = Device::find($request->session()->get('device_id'));
+            if(!empty($device)) {
+                if($device['is_admin'] === 1) {
+                    $isAdmin = true;
+                }
+            }
         }
-        $user = Auth::user();
-        if(!$user->isAdmin())
-        {
-            throw new AuthenticationException();
+
+        if($isAdmin === false) {
+            // 挡掉没权限的人
+            if(!Auth::check()) {
+                throw new AuthenticationException();
+            }
+            $user = Auth::user();
+            if(!$user->isAdmin())
+            {
+                throw new AuthenticationException();
+            }
         }
 
         $articles = DB::table(DB::raw('articles as a'))
